75c3c8a873ca46df9ef9ee001580777e192c9883
rooneyc <conor.p.rooney@aib.ie>
2017-04-09 21:04:33 +0100

    should_only_get_buy_two_get_one_free_discounts_once_for_four_articles passing

--- src/main/java/net/serenitybdd/dojo/supermarket/model/promotion/BuyToGetFree.java
+++ src/main/java/net/serenitybdd/dojo/supermarket/model/promotion/BuyToGetFree.java
@@ -5,20 +5,19 @@ import org.joda.money.Money;
 
 public class BuyToGetFree implements Promotion {
 
-    private int quantityMustBuy;
+    private int quantityMustBuyMoreThan;
     private int getFree;
 
     public BuyToGetFree(int quantityMustBuy, int getFree) {
-        this.quantityMustBuy = quantityMustBuy;
+        this.quantityMustBuyMoreThan = quantityMustBuy+1;
         this.getFree = getFree;
     }
 
     @Override
     public Money calculateDiscount(int quantityScanned, Money price) {
-        if (quantityScanned > quantityMustBuy) {
+        if (quantityScanned % quantityMustBuyMoreThan == 0) {
             return price.multipliedBy(getFree).negated();
         }
         return Money.parse("EUR 0.00");
     }
-
 }
